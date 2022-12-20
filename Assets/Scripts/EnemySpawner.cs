b4308c2729df0b8ef9cf5da7bc476e67f9857693
yucooong <roy0720@gmail.com>
2022-12-20 21:59:21 +0800

    房間加空氣牆判斷有沒有進入房間、進入房間後產生小怪、import香菇小怪模型

--- Assets/Scripts/EnemySpawner.cs
+++ Assets/Scripts/EnemySpawner.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class EnemySpawner : MonoBehaviour
 {
     public GameObject[] gameObjects;
+    private bool everentertheroom = false;
     //when player enter the room, create 3~6 enemy
     void Start()
     {
@@ -17,9 +18,9 @@ public class EnemySpawner : MonoBehaviour
     private void OnTriggerEnter(Collider other)
     {
     GameObject enemy;
-    Debug.Log("enter");
-        if(other.transform.name == "maincharacter")
+        if(other.transform.name == "maincharacter" && !everentertheroom)
         {
+            everentertheroom = true;
             int randomindex = Random.Range(0, gameObjects.Length);
             Vector3 randomposition = new Vector3(Random.Range(transform.position.x - 13f, transform.position.x - 1f), -1.5f, Random.Range(transform.position.z - 7f, transform.position.z + 8f));
 
