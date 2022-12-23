7034d07a1e3512c182bb765d310a74c6562be8da
海狸大師 <77geo5rge6@gmail.com>
2022-12-23 15:43:28 +0800

    新增暫停(esc)，調整蘑菇速度和生成位置

--- Assets/Scripts/EnemySpawner.cs
+++ Assets/Scripts/EnemySpawner.cs
@@ -23,7 +23,7 @@ public class EnemySpawner : MonoBehaviour
                 int randomindex = Random.Range(0, gameObjects.Length);
                 Vector3 randomposition = new Vector3(Random.Range(transform.position.x - 13f, transform.position.x - 1f), -1.5f, Random.Range(transform.position.z - 7f, transform.position.z + 8f));
                 enemy[i] = Instantiate(gameObjects[randomindex], randomposition, Quaternion.identity);
-                enemy[i].transform.parent = gameObject.transform;
+                //enemy[i].transform.parent = gameObject.transform; // 改用空氣牆座標來決定生成位置
             }//create 3~6 enemy within airwall
         }//judge who enter the room and has entered the room or not
     }//enter the room
--- Assets/Scripts/MushroomMonStatusController.cs
+++ Assets/Scripts/MushroomMonStatusController.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class MushroomMonStatusController : MonoBehaviour
 {
@@ -8,8 +9,7 @@ public class MushroomMonStatusController : MonoBehaviour
     private int currentHealth;
     public BarScript healthBar;
 
-    [SerializeField] int maxHealth = 100;
-    bool alive = true;
+    [SerializeField] int maxHealth = 45;
     bool now_is_sword;
     int playerDamage;
     float playerAttackRange;
@@ -22,15 +22,8 @@ public class MushroomMonStatusController : MonoBehaviour
 
     void Update()
     {
-
-        if (Input.GetMouseButtonDown(0) && IsSword() && IsCloseToPlayer())
-        {
-            TakeDamage(playerDamage);
-            Debug.Log("take damage");
-        }
-
+        PlayerAttackCheck();
         StatusCheck(); // check if the mushroom die
-        
     }
 
     // call the function below to change mushroom's health status
@@ -50,10 +43,10 @@ public class MushroomMonStatusController : MonoBehaviour
     {
         if (currentHealth <= 0)
         {
-            alive = false;
-            transform.localPosition += new Vector3(0, 0.1f, 0);
             transform.rotation = Quaternion.Euler(new Vector3(90, 0, 0)); // rotate the enemy's corpse (lying on the ground)
-            gameObject.GetComponent<CapsuleCollider>().enabled = false; // disable collider 
+            gameObject.GetComponent<CapsuleCollider>().enabled = false; // disable collider
+            gameObject.GetComponent<NavMeshAgent>().enabled = false; // disable navMeshAgent
+
             Invoke(nameof(DestroyMushroom), 3);
         }
     }
@@ -73,9 +66,14 @@ public class MushroomMonStatusController : MonoBehaviour
 
     bool IsCloseToPlayer()
     {
-        // todo
-        // check player layer sphere
-        return Physics.CheckSphere(transform.position, playerAttackRange, whatIsPlayer);
-        
+        return Physics.CheckSphere(transform.position, playerAttackRange, whatIsPlayer);   
+    }
+    void PlayerAttackCheck()
+    {
+        if (Input.GetMouseButtonDown(0) && IsSword() && IsCloseToPlayer())
+        {
+            GameObject.Find("maincharacter").transform.LookAt(transform);
+            TakeDamage(playerDamage);
+        }
     }
 }
