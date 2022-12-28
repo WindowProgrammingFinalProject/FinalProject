e6af5c056fe57c8c8a37b0fa31a18a98d129e106
海狸大師 <77geo5rge6@gmail.com>
2022-12-28 14:19:08 +0800

    修正回血罐(prefab: HealFlask, script: HealSpellScript) bug

---
+++ Assets/Prefabs/Health Spell/SpellScript.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpellScript : MonoBehaviour
+{
+
+    [SerializeField] Transform flask;
+    [SerializeField] public AudioSource audioSource;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        transform.Rotate(new Vector3(30, 0, 0));
+        audioSource.Stop();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(new Vector3(0, 90 * Time.deltaTime, 0));
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.name == "maincharacter")
+        {            
+            other.GetComponent<PlayerMovement>().addHealth();
+            audioSource.Play();
+            Invoke(nameof(DestroyObject), 0.3f);
+        }
+    }
+    void DestroyObject()
+    {
+        Destroy(gameObject);
+    }
+}
